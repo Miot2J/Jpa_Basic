9e14844159668bb3449f75a21bdfc7b4258970d9
Miot2J <ck96117@gmail.com>
2021-01-19 23:07:31 +0900

    Add README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Jpa_Basic
+
+-----
+
+### < Persistence Option >
++ property name="hibernate.show_sql" value="true" : sql 출력해줌
++ property name="hibernate.format_sql" value="true" : sql 보기쉽게 포맷해줌 
++ property name="hibernate.use_sql_comments" value="true" : 트랜잭션을 주석으로 보여줌 
+---
+
+< TIPS >
+
++ JPA를 시작할 때 EntityManagerFactory 를 선언해야 한다.
+  + persistenceUnitName 은 DB 마다 하나씩 고유 값을 가진다.(persistence.xml 내부에 선언)
++ Factory 에서 EntityManager 를 선언한다
++ JPA는 한 트랜잭션 안에서만 crud 해야한다.
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,17 +15,43 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
+        try {
+            //생성(입력코드)
+         /*   Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+            em.persist(member);
+            */
 
-        member.setId(1L);
-        member.setName("HelloA");
+            //수정 코드
+            /*  Member findMember = em.find(Member.class,1L);
+            findMember.setName("HelloJPA");*/
 
-        em.persist(member);
+            //삭제코드
+           /* Member findMember = em.find(Member.class,1L);
+              em.remove();
+            */
 
-        tx.commit();
 
-        em.close();
+            //전체 검색 코드(1~10까지)
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
 
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+            
+
+            
+            tx.commit();
+
+        } catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
         emf.close();
 
     }
