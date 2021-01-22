398c7e7f89200905d26f84bf4bbc42f8e22bcf80
Miot2J <ck96117@gmail.com>
2021-01-22 11:30:52 +0900

    Key Mapping

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,9 +6,15 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(name = "member_seq_generator",
+        sequenceName = "member_seq", //매핑할 DB 시퀀스 이름
+        initialValue = 1,
+        allocationSize = 50 //50개씩 미리 확보해놔서 동시성 이슈 해결한다.
+)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "member_seq_generator")
     private Long id;
 
     //객체는 username DB는 name 으로 표기하고 싶을때
