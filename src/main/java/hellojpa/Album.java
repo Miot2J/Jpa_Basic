c04058a1c1809b033d7f925811ca4dc1bb04eda9
Miot2J <ck96117@gmail.com>
2021-03-08 23:47:28 +0900

    update

--- src/main/java/hellojpa/Album.java
+++ src/main/java/hellojpa/Album.java
@@ -1,8 +1,10 @@
 package hellojpa;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("Album1")
 public class Album  extends Item{
 
     private String artist;
--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -1,10 +1,10 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn // default 는 DType으로 컬럼명이 들어온다
 public class Item {
 
     @Id
@@ -14,5 +14,27 @@ public class Item {
     private String name;
     private int price;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
